96b7f922e6dabcc08f6e03d0171326bbe1fdc639
alek <wait82985@gmail.com>
2023-05-11 13:07:30 +0600

    Add method that passes base test cases. 39 / 74 testcases passed

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,10 +5,17 @@ package org.example;
 // are uncrossed, can we write this as a recursion
 public class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        return 2;
+        int count = 0;
+        int newStart = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = newStart; j < nums2.length ; j++) {
+                if(nums1[i] == nums2[j]){
+                    count++;
+                    newStart = j + 1;
+                    break;
+                }
+            }
+        }
+        return count;
     }
-
-
-
-
 }
