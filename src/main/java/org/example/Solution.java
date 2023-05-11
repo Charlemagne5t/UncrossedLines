cb400f82bf798bec5178de722400dd377ddaa305
alek <wait82985@gmail.com>
2023-05-11 21:56:53 +0600

    Final commit. All tests passed.

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -5,32 +5,39 @@ package org.example;
 // are uncrossed, can we write this as a recursion
 public class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int[][] dp = new int[n + 1][m + 1];
 
-        return 3;
-    }
-    private int maxUncrossedLinesHelper(int[] nums1, int[] nums2) {
-        int count = 0;
-        int newStart = 0;
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = newStart; j < nums2.length ; j++) {
-                if(nums1[i] == nums2[j]){
-                    count++;
-                    newStart = j + 1;
-                    break;
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
                 }
             }
         }
-        int count2 = 0;
-        int newStart2 = 0;
-        for (int i = 0; i < nums2.length; i++) {
-            for (int j = newStart2; j < nums1.length ; j++) {
-                if(nums2[i] == nums1[j]){
-                    count2++;
-                    newStart2 = j + 1;
-                    break;
-                }
-            }
-        }
-        return Math.max(count2, count);
+
+
+        return dp[n][m];
     }
+
+    public static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
+
