7dc7f1642c6e448e242e690b31a09100948defff
alek <wait82985@gmail.com>
2023-05-11 14:12:27 +0600

    create 4th test

--- src/test/java/TestSolution.java
+++ src/test/java/TestSolution.java
@@ -19,6 +19,6 @@ public class TestSolution {
 
     @Test
     public void maxUncrossedLinesTest4() {
-        Assert.assertEquals(3, new Solution().maxUncrossedLines(new int[]{1,1,3,5,3,3,5,5,1,1}, new int[]{2,3,2,1,3,5,3,2,2,1}));
+        Assert.assertEquals(5, new Solution().maxUncrossedLines(new int[]{1,1,3,5,3,3,5,5,1,1}, new int[]{2,3,2,1,3,5,3,2,2,1}));
     }
 }
